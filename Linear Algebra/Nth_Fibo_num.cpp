79760384ccbd38b8f163d145bf8cedff49c484fd
Abhijeet289 <44442689+Abhijeet289@users.noreply.github.com>
2021-05-31 22:56:40 +0530

    Create Nth_Fibo_num.cpp

---
+++ Linear Algebra/Nth_Fibo_num.cpp
@@ -0,0 +1,10 @@
+const ll MOD = 1000000007;
+unordered_map<ll,ll> Fib;
+ 
+ll fib(ll n)
+{
+    if(n<2) return 1;
+    if(Fib.find(n) != Fib.end()) return Fib[n];
+    Fib[n] = (fib((n+1) / 2)*fib(n/2) + fib((n-1) / 2)*fib((n-2) / 2)) % MOD;
+    return Fib[n];
+}
